b5c1af2f1700a8abfb0e7c3c869ae90a288516d9
Khaled Alwan <khaled_alwan@hotmail.com>
2016-11-01 11:23:47 +0000

    uploaded files.
    contains a C# class for testing and a unit testing module.

---
+++ MSunitTest/MSunitTest/DESencDec.cs
@@ -0,0 +1,81 @@
+﻿// (c) Khaled A Alwan .
+// All other rights reserved.
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+/// <summary>
+/// A sample encrypt / decrypt functions, for unit testing purposes.
+/// </summary>
+
+public class DESencDec
+{
+    string key = "!$%^*@X0";
+    /// <summary>
+    /// Encrypt the Given file using the DES algo
+    /// output the result into the given filename.
+    /// </summary>
+    /// <param name="InFile">The plain text file name.</param>
+    /// <param name="OutFile">encrypted output file name.</param>
+    public void Encrypt(string InFile, string OutFile)
+    {
+        //Create a reader stream
+        FileStream inpStream = new FileStream(InFile, FileMode.Open, FileAccess.Read);
+        //Create Encryptor Stream output.
+        FileStream EncryptedStream = new FileStream(OutFile, FileMode.Create, FileAccess.Write);
+        // initialize the Encryptor to transform.
+        ICryptoTransform desencrypt = getED(true);
+        //Set-up the Encryptor Stream Transformer.
+        CryptoStream cryptostream = new CryptoStream(EncryptedStream, desencrypt, CryptoStreamMode.Write);
+        //Perform the transformation.
+        byte[] bytearrayinput = new byte[inpStream.Length];
+        inpStream.Read(bytearrayinput, 0, bytearrayinput.Length);
+        cryptostream.Write(bytearrayinput, 0, bytearrayinput.Length);
+        //Close all streams.
+        cryptostream.Close(); cryptostream.Dispose();
+        inpStream.Close(); inpStream.Dispose();
+        EncryptedStream.Close(); EncryptedStream.Dispose();
+        desencrypt.Dispose();
+    }
+    /// <summary>
+    /// Decrypt the Given file using the DES algo
+    /// output the plain text into the given filename.
+    /// </summary>
+    /// <param name="sInputFilename">the encrypted file name.</param>
+    /// <param name="sOutputFilename">plain text output file name.</param>
+    public void Decrypt(string sInputFilename, string sOutputFilename)
+    {
+        // initialize the Decryptor to transform.
+        ICryptoTransform desdecrypt = getED(false);
+
+        //Create a file stream to read the encrypted file back.
+        FileStream fsread = new FileStream(sInputFilename, FileMode.Open, FileAccess.Read);
+        //Set-up the Decryptor to start work.
+        CryptoStream cryptostreamDecr = new CryptoStream(fsread, desdecrypt, CryptoStreamMode.Read);
+        //init the output stream.
+        StreamWriter fsDecrypted = new StreamWriter(sOutputFilename);
+        //write the contents of the decrypted file.
+        fsDecrypted.Write(new StreamReader(cryptostreamDecr).ReadToEnd());
+        fsDecrypted.Flush();
+        fsDecrypted.Close();fsDecrypted.Dispose();
+        fsread.Close();fsread.Dispose();
+        desdecrypt.Dispose();
+    }
+    /// <summary>
+    /// initialize an Encryptor / Decryptor class
+    /// </summary>
+    /// <param name="enc">true/false to Encrypt/Decrypt</param>
+    /// <returns>Return the Encryptor / Decryptor transform</returns>
+    private ICryptoTransform getED(bool enc)
+    {
+        DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+        DES.Key = ASCIIEncoding.ASCII.GetBytes(key);
+        DES.IV = ASCIIEncoding.ASCII.GetBytes(key);
+        ICryptoTransform desencrypt = null;
+        if (enc == true)
+            desencrypt = DES.CreateEncryptor();
+        else
+            desencrypt = DES.CreateDecryptor();
+        return desencrypt;
+    }
+}
---
+++ MSunitTest/MSunitTestTests/DESencDecTests.cs
@@ -0,0 +1,89 @@
+﻿// (c) Khaled A Alwan .
+// All other rights reserved.
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class DESencDecTests
+    {
+        const string orPlain = "original.txt";//input plain text
+        const string ExPlain = "expected.txt";//test plain output text should match orignal after decrypt is done.
+        const string Encrypted = "Encrypted"; //the encryption results save here.
+        public DESencDecTests()
+        {
+
+        }
+
+        [TestMethod()]
+        [Priority(0)]
+        public void CleanPrevTest()
+        {
+            if (File.Exists(orPlain) == true)
+                File.Delete(orPlain);
+            if (File.Exists(ExPlain) == true)
+                File.Delete(ExPlain);
+            if (File.Exists(Encrypted) == true)
+                File.Delete(Encrypted);
+        }
+
+        [TestMethod()]
+        [Priority(1)]
+        public void makePlainTextTestFile()
+        {
+            StreamWriter sw = new StreamWriter(orPlain);
+            for (int i = 0; i < 10; i++)
+            {
+                sw.Write("testing, testing, {0}{1}{2}", i, i, i);
+                sw.Write("yet another line");
+            }
+            sw.Close(); sw.Dispose();
+        }
+
+        [TestMethod()]
+        [Priority(10)]
+        public void EncryptTest()
+        {
+            Assert.IsTrue(File.Exists(orPlain));
+            Assert.IsTrue(File.ReadAllBytes(orPlain).Length > 0);
+            DESencDec test = new DESencDec();
+            test.Encrypt(orPlain, Encrypted);
+            Assert.IsTrue(File.Exists(Encrypted));
+            Assert.IsTrue(File.ReadAllBytes(Encrypted).Length > 0);
+        }
+
+        [TestMethod()]
+        [Priority(11)]
+        public void DecryptTest()
+        {
+            Assert.IsTrue(File.Exists(orPlain));
+            Assert.IsTrue(File.ReadAllBytes(orPlain).Length > 0);
+
+            DESencDec test = new DESencDec();
+
+
+            test.Decrypt(Encrypted, ExPlain);
+
+            Assert.IsTrue(File.Exists(ExPlain));
+            Assert.IsTrue(File.ReadAllBytes(ExPlain).Length > 0);
+
+            StreamReader orignal = new StreamReader(orPlain);
+            StreamReader expected = new StreamReader(ExPlain);
+
+            string orignalStr = orignal.ReadToEnd(); orignal.Close(); orignal.Dispose();
+            string expectedStr = expected.ReadToEnd(); expected.Close(); expected.Dispose();
+
+            Assert.AreEqual(orignalStr, expectedStr);
+        }
+
+      
+
+
+    }
+}
